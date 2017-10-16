800f416b7a49163df2cfa1381293c92af38aad6c
Galacticaa <galacticaa@users.noreply.github.com>
2017-10-16 15:15:44 +0100

    Add 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,22 @@
+@extends ('adminlte::page')
+
+@section ('title', 'Page not Found')
+
+@section ('content_header')
+<ol class="breadcrumb">
+    <li><a href="/">Home</a></li>
+    <li class="active">Page not found</li>
+</ol>
+@stop
+
+@section ('content')
+<div class="error-page">
+    <h2 class="headline text-yellow">404</h2>
+    <div class="error-content">
+        <h3>
+            <i class="fa fa-warning text-yellow"></i>
+            Oops! This page doesn't exist.
+        </h2>
+    </div>
+</div>
+@stop
