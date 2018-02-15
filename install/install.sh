f5a2468f244b221b9ae012c9df3610fd54a4d0c0
Galacticaa <galacticaa@users.noreply.github.com>
2018-02-15 21:29:07 +0000

    Add Curl, GD and Zip extensions to PHP

--- install/install.sh
+++ install/install.sh
@@ -45,7 +45,7 @@ pip install --upgrade pip
 pip install virtualenv
 
 . mysql/install.sh
-apt-get install -y php7.1 php7.1-fpm php7.1-mbstring php7.1-mysql php7.1-xml
+apt-get install -y php7.1 php7.1-curl php7.1-fpm php7.1-gd php7.1-mbstring php7.1-mysql php7.1-xml php7.1-zip
 wget -4 https://getcomposer.org/installer && php installer --install-dir=/usr/local/bin --filename=composer
 
 
