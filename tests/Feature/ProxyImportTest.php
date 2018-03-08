7d8a036036a787adb5b95a42634840428919efa0
Galacticaa <galacticaa@users.noreply.github.com>
2018-03-08 01:12:01 +0000

    Add tests for actually importing proxies

--- tests/Feature/ProxyImportTest.php
+++ tests/Feature/ProxyImportTest.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ProxyImportTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function theImportModalHasAllFields()
     {
@@ -16,6 +18,58 @@ class ProxyImportTest extends TestCase
         $this->assertAllImportFieldsAreVisible($response);
 
         $this->assertAllProxyProvidersAreVisible($response);
+    }
+
+    /** @test */
+    public function canImportProxies()
+    {
+        $response = $this->from('proxies')->post('proxies/import', [
+            'provider' => 'lime',
+            'mode' => 'a',
+            'proxies' => implode("\n", [
+                "127.0.0.1",
+                "192.168.1.100",
+            ]),
+        ]);
+
+        $response->assertRedirect('proxies');
+
+        $this->assertDatabaseHas('proxies', [
+            'url' => '192.168.1.100',
+            'provider' => 'lime',
+        ]);
+
+        $this->assertDatabaseHas('proxies', [
+            'url' => '127.0.0.1',
+            'provider' => 'lime',
+        ]);
+    }
+
+    /** @test */
+    public function proxyListShowsNewProxiesAfterImport()
+    {
+        $response = $this->from('proxies')->post('proxies/import', [
+            'provider' => 'lime',
+            'mode' => 'a',
+            'proxies' => implode("\n", [
+                "127.0.0.1",
+                "192.168.1.100",
+            ]),
+        ]);
+
+        $response = $this->get('proxies');
+
+        $this->seesTableHeaders($response, [
+            'Proxy',
+            'Provider',
+            'Map Area',
+            'PTC Status',
+            'PoGo Status',
+        ]);
+
+        $response->assertSee('Lime Proxies');
+        $response->assertSee('127.0.0.1');
+        $response->assertSee('192.168.1.100');
     }
 
     protected function assertAllImportFieldsAreVisible($response)
@@ -40,6 +94,15 @@ class ProxyImportTest extends TestCase
             'My Private Proxy',
             'ProxyPrivate',
         ]);
+    }
+
+    protected function seesTableHeaders($response, array $values)
+    {
+        foreach ($values as $k => $v) {
+            $values[$k] = '<th>'.$v.'</th>';
+        }
+
+        $this->seesValues($response, $values);
     }
 
     protected function seesValues($response, array $values)
